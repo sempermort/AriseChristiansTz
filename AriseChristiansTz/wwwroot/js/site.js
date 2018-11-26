d0a45a8e610386332e5e87f3f8eaf2e1ea67d307
sempermort <semperjack@gmail.com>
2018-11-26 20:21:34 +0300

    1.0 changes

--- AriseChristiansTz/wwwroot/js/site.js
+++ AriseChristiansTz/wwwroot/js/site.js
@@ -1,1 +1,14 @@
 ﻿// Write your JavaScript code.
+$(document).ready(function () {
+    // Mega dropdown menu.
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideDown("400");
+            $(this).toggleClass('open');
+        },
+        function () {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true, true).slideUp("400");
+            $(this).toggleClass('open');
+        }
+    );
+});
